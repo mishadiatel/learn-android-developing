ad50a075e5c1cc2e0ae6ed86c5ae45d131820d1e
mishadiatel <mishadiatel13@gmail.com>
2022-06-19 15:19:11 +0300

    added Bottom navigation to random

--- Random/app/src/main/java/com/example/yes_no/MainActivity.kt
+++ Random/app/src/main/java/com/example/yes_no/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.yes_no
 
-import android.annotation.SuppressLint
-import android.graphics.Color
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import com.example.yes_no.databinding.ActivityMainBinding
 import java.util.concurrent.ThreadLocalRandom
 import kotlin.random.Random
@@ -22,10 +17,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.bNav?.selectedItemId = R.id.btnyasno1
+        binding.bNav?.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.btnyasno1 -> {
+                    bool = true
+                    binding.apply {
+                        tvRandom.text = ""
+                        button.visibility = View.VISIBLE
+                        btn1min.visibility = View.INVISIBLE
+                        btn1plus.visibility = View.INVISIBLE
+                        btn2min.visibility = View.INVISIBLE
+                        btn2plus.visibility = View.INVISIBLE
+                        lnumb.visibility = View.INVISIBLE
+                        fnumb.visibility = View.INVISIBLE
+                    }
+                }
+                R.id.btnrandomnum1 ->{
+                    bool = false
+                    binding.apply {
+                        tvRandom.text = ""
+                        button.visibility = View.VISIBLE
+                        btn1min.visibility = View.VISIBLE
+                        btn1plus.visibility = View.VISIBLE
+                        btn2min.visibility = View.VISIBLE
+                        btn2plus.visibility = View.VISIBLE
+                        lnumb.visibility = View.VISIBLE
+                        fnumb.visibility = View.VISIBLE
+                    }
+                }
+            }
+            true
+        }
     }
 
-    @SuppressLint("ResourceAsColor")
-    @RequiresApi(Build.VERSION_CODES.M)
     fun random(view: View){
         if (bool == true){
             val count = Random.nextBoolean()
@@ -44,40 +69,6 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, getString(R.string.error), Toast.LENGTH_SHORT).show()
                 }
             }
-        }
-    }
-    @SuppressLint("ResourceAsColor")
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun onClickYesNo(view: View){
-        bool = true
-        binding.apply {
-            tvRandom.text = ""
-            button.visibility = View.VISIBLE
-            btn1min.visibility = View.INVISIBLE
-            btn1plus.visibility = View.INVISIBLE
-            btn2min.visibility = View.INVISIBLE
-            btn2plus.visibility = View.INVISIBLE
-            lnumb.visibility = View.INVISIBLE
-            fnumb.visibility = View.INVISIBLE
-            btnyasno.setTextColor(R.color.btnColor)
-            btnrandomnum.setTextColor(Color.WHITE)
-        }
-    }
-    @SuppressLint("ResourceAsColor")
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun onClickNumbers(view: View) {
-        bool = false
-        binding.apply {
-            tvRandom.text = ""
-            button.visibility = View.VISIBLE
-            btn1min.visibility = View.VISIBLE
-            btn1plus.visibility = View.VISIBLE
-            btn2min.visibility = View.VISIBLE
-            btn2plus.visibility = View.VISIBLE
-            lnumb.visibility = View.VISIBLE
-            fnumb.visibility = View.VISIBLE
-            btnrandomnum.setTextColor(R.color.btnColor)
-            btnyasno.setTextColor(Color.WHITE)
         }
     }
     fun onClickBtn1plus(view: View){
@@ -113,6 +104,4 @@ class MainActivity : AppCompatActivity() {
         lnumb.text = savedInstanceState.getString("lnumb")
         fnumb.text = savedInstanceState.getString("fnumb")
     }
-    
-
 }
